6d99e8b92aff3f7a5377c8eb373c6ebbbca735b9
David Vitrant <david@synteny.us>
2013-12-09 20:13:59 +0000

    python code being saved for posterity

--- pru_vide.py
+++ pru_vide.py
@@ -11,15 +11,13 @@ if __name__ == '__main__':
     mcp = Adafruit_MCP230XX(address = 0x20, num_gpios = 16) # MCP23017
 
     # Set pins 0, 1 and 2 to output (you can set pins 0..15 this way)
-#    mcp.config(0, mcp.OUTPUT)
-#    mcp.config(1, mcp.OUTPUT)
-    mcp.config(4, mcp.OUTPUT)
-    mcp.config(3, mcp.OUTPUT)
-    mcp.output(4, 0)
+#    mcp.config(4, mcp.OUTPUT) #LED if present
+    mcp.config(7, mcp.OUTPUT) #SSR sous vide
+    mcp.config(3, mcp.OUTPUT) 
 
     # Set pin 3 to input with the pullup resistor enabled
-    mcp.pullup(6, 1)
-    mcp.pullup(5, 1)
+    mcp.pullup(2, 1) #Bottom button
+    mcp.pullup(1, 1) #Second to bottom button
 
     # Read input pin and display the results
   #  print "Pin 3 = %d" % (mcp.input(6) >> 3)
@@ -27,11 +25,13 @@ if __name__ == '__main__':
     # Python speed test on output 0 toggling at max speed
     print "Starting blinky on pin 0 (CTRL+C to quit)"
     while (True):
-        print "Pin 5 = %d" % (mcp.input(5))
-        print "Pin 6 = %d" % (mcp.input(6))
- #       mcp.output(4, 1)  # Pin 0 High
+        print "Pin 5 = %d" % (mcp.input(2))
+        print "Pin 6 = %d" % (mcp.input(1))
+ #       mcp.output(4, 1)  # Pin 0 High for led
         mcp.output(3, 1)
+        mcp.output(7, 1)
         time.sleep(1);
   #      mcp.output(4, 0)  # Pin 0 Low
         mcp.output(3, 0)
+        mcp.output(7, 0)
         time.sleep(1);
