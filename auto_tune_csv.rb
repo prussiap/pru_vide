6073bad854a46de25a00eab6af827d3d262b8730
David Vitrant <david@synteny.us>
2013-12-16 18:58:20 +0000

    added auto_tune csv

---
+++ auto_tune_csv.rb
@@ -0,0 +1,33 @@
+require_relative 'lib/heating_element'
+require "rubygems"
+#require "google_drive"
+require 'csv'
+require_relative "lib/temperature_sensor"
+require 'pi_piper'
+include PiPiper
+
+
+setpoint = 50.0
+temp = TemperatureSensor.new
+start_time = Time.now
+pin_status = 0
+pin = PiPiper::Pin.new(:pin => 17, :direction => :out)
+pin.on
+
+my_file = CSV.open("#{Time.now}_autotune.csv", 'wb')
+
+i = 2
+while true
+	pv = temp.get_temp
+	if pv.to_f > setpoint
+		pin.off
+		pin_status = 0
+	else
+		pin.on
+		pin_status = 1
+	end
+	my_file << [ setpoint, pv, pin_status, (start_time - Time.now) ]
+	p "Current temp: #{pv}, Setpoint: #{setpoint}, Status: #{pin_status}"
+	i += 1
+	sleep(5)
+end
