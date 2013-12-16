02eff0f17d6dcfb3e9b0cbc84ca4a889b2bc9c7d
David Vitrant <david@synteny.us>
2013-12-16 18:55:14 +0000

    basic auto-tuning

--- google_docs.rb
+++ google_docs.rb
@@ -14,20 +14,23 @@ session = GoogleDrive.login("david@synteny.us", "Aquafina1")
 #https://docs.google.com/spreadsheet/ccc?key=0AgyFdwj_tPl_dG5pOXBjRWMxWGF1ZHRXNGxzTHVzZmc&usp=sharing
 ws = session.spreadsheet_by_key("0AgyFdwj_tPl_dG5pOXBjRWMxWGF1ZHRXNGxzTHVzZmc").worksheets[0]
 
-temp = TemperatureSensor.new.get_temp
+temp = TemperatureSensor.new
 start_time = Time.now
 pin = PiPiper::Pin.new(:pin => 17, :direction => :out)
 pin.on
 # Changes content of cells.
 # Changes are not sent to the server until you call ws.save().
 
-i = 1
+i = 2
 while true
+	current_temperature = temp.get_temp
 	ws[i, 1] = 100
-	ws[i, 2] = temp
+	ws[i, 2] = current_temperature
 	ws[i, 3] = "on"
 	ws[i, 4] = (start_time - Time.now)
 	ws.save()
+	i += 1
+	p "Current temperature: #{current_temperature}"
 end
 
 ws.reload()
