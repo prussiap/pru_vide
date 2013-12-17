d13b46bb25ea797ba6c8de3ab6e38d70b9526773
David Vitrant <david@synteny.us>
2013-12-17 21:51:08 +0000

    tweaking steady state heater test script and test data

--- auto_tune.rb
+++ auto_tune.rb
@@ -1,9 +1,38 @@
-require_relative 'heating_element'
-require 'temper'
-require 'CSV'
+require_relative 'lib/heating_element'
+require "rubygems"
+require "google_drive"
+require_relative "lib/temperature_sensor"
+require 'pi_piper'
+include PiPiper
+
+session = GoogleDrive.login("david@synteny.us", "Aquafina1")
+ws = session.spreadsheet_by_key("0AgyFdwj_tPl_dG5pOXBjRWMxWGF1ZHRXNGxzTHVzZmc").worksheets[2]
+setpoint = 50
+temp = TemperatureSensor.new
+start_time = Time.now
+pin_status = 0
+pin = PiPiper::Pin.new(:pin => 17, :direction => :out)
+pin.off
 
 
-file = CSV.open( "#{Time.now}_tuning\.csv", 'wb')
+i = 2
+while true
+	pv = temp.get_temp
+	if pv.to_i > setpoint
+		pin.off
+		pin_status = 0
+	else
+		p "else"
+		pin.on
+		pin_status = 1
+	end
+	ws[i, 1] = setpoint
+	ws[i, 2] = pv
+	ws[i, 3] = pin_status
+	ws[i, 4] = (start_time - Time.now)
+	ws.save()
+	i += 1
+	p "Current temp: #{pv}, Setpoint: #{setpoint}, Status: #{pin_status}"
+	sleep(5)
+end
 
-
-file << [setpoint, current_temperature, output, time]
--- steady_state_heater.rb
+++ steady_state_heater.rb
@@ -5,30 +5,32 @@ require 'csv'
 require_relative "lib/temperature_sensor"
 require 'pi_piper'
 include PiPiper
-
+require 'wiringpi'
 
 temp = TemperatureSensor.new
 start_time = Time.now
 pin_status = 0
 pin = PiPiper::Pin.new(:pin => 17, :direction => :out)
-# pin.on
-power = HeatingElement.new adapter: pin
-power.pulse_width = 3000
+pin.off
+power = HeatingElement.new pin
+power.pulse_width = 2000
 
-run_1_time = start_time + (45 * 60)
+run_1_time = start_time + (10 * 60)
 
 my_file = CSV.open("#{Time.now}_steady_state_heater.csv", 'wb')
 
 loop do
+	pin_status = pin.read
+	pv = temp.get_temp
 	if Time.now < run_1_time
 		power.pulse
-		my_file << [ setpoint, pv, pin_status, (start_time - Time.now) ]
+		my_file << [ pv, pin_status, (Time.now - start_time)/60]
 		p "Current temp: #{pv}, Status: #{pin_status}"
 		sleep 1
 	else
-		power.pulse_width = 4000
+		power.pulse_width = 3000
 		power.pulse
-		my_file << [ setpoint, pv, pin_status, (start_time - Time.now) ]
+		my_file << [ pv, pin_status, (start_time - Time.now) ]
 		p "Current temp: #{pv}, Status: #{pin_status}"
 		sleep 1
 	end
