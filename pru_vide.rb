29f357993310fb3fbdea03b913a7d8eb1b58c3a1
David Vitrant <david@synteny.us>
2013-12-30 23:28:50 +0000

    made some changes to heating element and pruvide

--- pru_vide.rb
+++ pru_vide.rb
@@ -1,6 +1,6 @@
-require_relative 'temper_control'
+require_relative 'lib/temp_control'
 
-control = TempControl.new target: 152.0
+control = TempControl.new target: 57.2, pulse_range: 10000, kp: 4.5, ki: 110, kd: 27.5
 loop do
  	control.control_cycle
 end
