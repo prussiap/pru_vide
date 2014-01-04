5b8080775d13c4944b23bffc196a9911af8f71bf
Patrick Kennedy <bigredtriangle@gmail.com>
2014-01-04 16:04:34 -0500

    added some comments to hopefully make code easier to figure out

--- em_pru_vide.rb
+++ em_pru_vide.rb
@@ -14,16 +14,21 @@ context = EM::ZeroMQ::Context.new(1)
 EM.run {
   push_socket = context.socket(ZMQ::PUB)
   push_socket.bind("tcp://127.0.0.1:6000")
+
+  # default values for startup -- for now, needs to be set manually by
+  # SSHing in.
   control = TempControl.new target: 57.2, pulse_range: 10000, kp: 4.5, ki: 110, kd: 27.5
 
   rep_socket = context.socket(ZMQ::REP)
   rep_socket.connect("tcp://127.0.0.1:5000")
 
+  # takes a hash from JSON.
+  # to update target temperature, hash needs a "setpoint" key
+  # and the new target temperature as it's associated value.
+  # to retrieve menu listing hash, send a "menu" key, value is unused.
   rep_sock.on(:message) { |part|
     parsed_message = JSON.parse(part.copy_out_string)
-    if parsed_message["msg"]
-      puts parsed_message["msg"]
-    elsif parsed_message['setpoint']
+    if parsed_message['setpoint']
       rep_sock.send_msg('received setpoint')
       control.target = parsed_message['setpoint']
       puts "new target #{control.target}"
@@ -33,11 +38,12 @@ EM.run {
     part.close
   }
 
-
+  # loops control cycle
   EM.add_periodic_timer(0) {
     control.control_cycle
   }
 
+  # sends current observed temperature via PUB socket
   EM.add_periodic_timer(0.1) {
     push_socket.send_msg(control.last_reading)
   }
